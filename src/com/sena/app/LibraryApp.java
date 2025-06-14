cf06871d503313b24616b0691d45789c30afb31f
AdrianCMT <adcamoti@gmail.com>
2025-06-13 20:40:16 -0500

    main() y base App

---
+++ src/com/sena/app/LibraryApp.java
@@ -0,0 +1,18 @@
+package com.sena.app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import com.sena.app.models.Book;
+
+public class LibraryApp {
+    private static Scanner scanner = new Scanner(System.in);
+    private static List<Book> library = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("🚀 Biblioteca Digital - Versión 1.0");
+
+        // TODO: Implementar menú básico
+        scanner.close();
+    }
+}
